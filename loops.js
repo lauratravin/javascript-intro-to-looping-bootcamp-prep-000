98d0cee73925a95e2456a4ae069089c01b747fd1
Laura Travin <laura.travin@gmail.com>
2017-11-07 22:12:39 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,14 +1,12 @@
 var myArray;
+
+
 function forLoop(myArray){
   for (let i = 0; i < 25; i++){
      if (i == 1){
-       myArray = ['I am 1 strange loop.'];
+       myArray.push('I am 1 strange loop.');
       } else {
-       console.log("I am ${i} strange loops.");
+       myArray.push('I am ${i} strange loops.');
      }
   }
-
-
-
-
 }
